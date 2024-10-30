1e8738aac9bbe72665d9eb54f183c519d195d447
Prathap Mullaguri <prathap.mullaguri@nrg.com>
2024-10-30 08:54:32 +0530

    OSGI Configs

--- core/src/main/java/com/nrgtraining/core/services/impl/ReadDataFromAPIImpl.java
+++ core/src/main/java/com/nrgtraining/core/services/impl/ReadDataFromAPIImpl.java
@@ -6,18 +6,44 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.propertytypes.ServiceRanking;
+import org.osgi.service.metatype.annotations.Designate;
 
+import com.nrgtraining.core.configurations.TrainingConfigurations;
 import com.nrgtraining.core.services.ReadDataFromAPI;
 
 @Component(service = ReadDataFromAPI.class, immediate = true)
 @ServiceRanking(value = 999)
+@Designate(ocd = com.nrgtraining.core.configurations.TrainingConfigurations.class)
 public class ReadDataFromAPIImpl implements ReadDataFromAPI {
 
+	private String url;
+	
+	@Activate
+	public void activate(TrainingConfigurations TrainingConfiguration)
+	{
+		url= TrainingConfiguration.geturl();
+	}
+	
+	@Deactivate
+	public void deActivate(TrainingConfigurations TrainingConfiguration)
+	{
+		url= "";
+	}
+	
+	@Modified
+	public void modified(TrainingConfigurations TrainingConfiguration)
+	{
+		url= TrainingConfiguration.geturl();
+	}
+	
 	@Override
 	public String getAPIData() throws Exception{
-		URL dummyJsonURL = new URL("https://dummyjson.com/todos");
+		URL dummyJsonURL = new URL(url);
 		StringBuilder responseBuilder = new StringBuilder();
 		HttpURLConnection httpCon = (HttpURLConnection) dummyJsonURL.openConnection();
 		httpCon.setDoOutput(true);
